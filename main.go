588860e253ced2f50ce5a7ced2d5285366310db4
Jackson Welsh <me@jacksonwel.sh>
2023-04-25 21:29:28 -0500

    add unsetenv as well

--- main.go
+++ main.go
@@ -80,6 +80,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 		return builtins.EnvironmentVariables(w, args...)
 	case "pwd":
 		return builtins.PresentWorkingDirectory(w, args...)
+	case "unsetenv":
+		return builtins.UnsetEnv(args...)
 	case "exit":
 		exit <- struct{}{}
 		return nil
